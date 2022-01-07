91f588e3c9009aa5e4776c66d09b59177981d60d
alcir09 <alcir.amorim9@gmail.com>
2022-01-07 11:28:09 -0300

    Adicionando matriz

--- Program.cs
+++ Program.cs
@@ -6,16 +6,36 @@ namespace Colecoes{
 
         static void Main(string[] args){
 
-            int[] arrayInteiros = new int[3];
+            int[,] matriz = new int[4,2]{
 
-            arrayInteiros[0] = 10;
-            arrayInteiros[1] = 20;
-            arrayInteiros[2] = 30;
+                { 8 , 8   },
+                { 10, 20  },
+                { 50, 100 },
+                {90 , 200 }
+            }; 
 
-             for(int i = 0; i < arrayInteiros.Length; i++){
+            for(int i = 0; i < matriz.GetLength(0); i++){
 
-                 System.Console.WriteLine(arrayInteiros[i]);
-             }
+                for (int l = 0; l < matriz.GetLength(1); l++){
+
+                  System.Console.WriteLine(matriz[i,l]);
+
+                }
+            }
+
+            // int[] arrayInteiros = new int[3];
+
+            // arrayInteiros[0] = 10;
+            // arrayInteiros[1] = 20;
+            // arrayInteiros[2] = int.Parse("30");
+
+            // //
+            // arrayInteiros[3] = 30;
+
+            //  for(int i = 0; i < arrayInteiros.Length; i++){
+
+            //      System.Console.WriteLine(arrayInteiros[i]);
+            //  }
         }
     }
 
