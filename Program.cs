2d959bafabfb2f1111f900d94e75dee3f659248a
alcir09 <alcir.amorim9@gmail.com>
2022-01-07 12:20:22 -0300

    Ordeando Array

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Colecoes.Helper;
 
 namespace Colecoes{
 
@@ -6,22 +7,36 @@ namespace Colecoes{
 
         static void Main(string[] args){
 
-            int[,] matriz = new int[4,2]{
+            OperacoesArray op = new OperacoesArray();
+            
+            int[] array = new int[5] {6,3,8,1,9};
 
-                { 8 , 8   },
-                { 10, 20  },
-                { 50, 100 },
-                {90 , 200 }
-            }; 
+            System.Console.WriteLine("Array original");
+            op.ImprimirArray(array);
 
-            for(int i = 0; i < matriz.GetLength(0); i++){
+            //op.OrdenarBubbleSort(ref array);
 
-                for (int l = 0; l < matriz.GetLength(1); l++){
+            op.Ordenar(ref array);
 
-                  System.Console.WriteLine(matriz[i,l]);
+            System.Console.WriteLine("Array ordenado");
+            op.ImprimirArray(array);
 
-                }
-            }
+            // int[,] matriz = new int[4,2]{
+
+            //     { 8 , 8   },
+            //     { 10, 20  },
+            //     { 50, 100 },
+            //     {90 , 200 }
+            // }; 
+
+            // for(int i = 0; i < matriz.GetLength(0); i++){
+
+            //     for (int l = 0; l < matriz.GetLength(1); l++){
+
+            //       System.Console.WriteLine(matriz[i,l]);
+
+            //     }
+            // }
 
             // int[] arrayInteiros = new int[3];
 
